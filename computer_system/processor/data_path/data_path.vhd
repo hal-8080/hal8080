f488d40e72bf8e76b17442a4609530d951e0c829
Dennis8Hu <dennis8hu@gmail.com>
2020-10-26 17:18:42 +0100

    Added structure for mux and decoder, OP1 of screen and branch/sethi still missing

--- computer_system/processor/data_path/data_path.vhd
+++ computer_system/processor/data_path/data_path.vhd
@@ -4,7 +4,7 @@ ENTITY data_path IS
 	PORT(
 		clk			: IN 	std_logic;
 		reset			: IN 	std_logic;
-<<<<<<< HEAD
+
 	-- From the controller
 		micro_inst 	: IN 	std_logic_vector(25 DOWNTO 0);	
 	-- To the controller
@@ -13,69 +13,95 @@ ENTITY data_path IS
 		mmI			: IN 	std_logic_vector(15 DOWNTO 0);
 		mmAdress		: OUT std_logic_vector(15 DOWNTO 0);
 		mmData		: OUT std_logic_vector(15 DOWNTO 0);
-		ir				: OUT std_logic_vector(15 DOWNTO 0);
-=======
-		instruction : IN 	std_logic_vector(15 DOWNTO 0);
-		mmI			: IN 	std_logic_vector(15 DOWNTO 0);
-		statusN		: OUT std_logic;
-		statusZ		: OUT std_logic;
-		mmAdress		: OUT std_logic_vector(15 DOWNTO 0);
-		mmData		: OUT std_logic_vector(15 DOWNTO 0);
 		ir				: OUT std_logic_vector(15 DOWNTO 0)
->>>>>>> 5eb785d629fcf1c670645506ea261df98e6df8db
 		
 	);
 END ENTITY data_path;
+
+
+
 ARCHITECTURE bhv OF data_path IS
-<<<<<<< HEAD
 	
 	TYPE reg_vector IS ARRAY (0 to 31) OF std_logic_vector(15 DOWNTO 0); --array of 32 16-bit vectors
 
 	SIGNAL reg : reg_vector; --32 registers of 16 bits
 										--use example: "reg(2) <= a_16_bit_vector" stores the vector in register 2
 -- Split up the micro instruction									
-	SIGNAL addrA	: std_logic_vector(4 DOWNTO 0) 	:= micro_inst(25 DOWNTO 21)
-	SIGNAL addrB	: std_logic_vector(4 DOWNTO 0) 	:= micro_inst(19 DOWNTO 15)
-	SIGNAL muxA		: std_logic 							:= micro_inst(20)				-- 0 then take MIR A or 1 take from %r 
-	SIGNAL muxB		: std_logic 							:= micro_inst(14)
-	SIGNAL muxC		: std_logic 							:= micro_inst(15)				-- To either store in registers or mm
-	SIGNAL rd		: std_logic 							:= micro_inst(12)
-	SIGNAL wr		: std_logic 							:= micro_inst(11)
-	SIGNAL cond		: std_logic_vector(2 DOWNTO 0) 	:= micro_inst(10 DOWNTO 8)
-	SIGNAL jump		: std_logic_vector(7 DOWNTO 0) 	:= micro_inst(7 DOWNTO 0)
+	SIGNAL micro_addrA	: std_logic_vector(4 DOWNTO 0) 	:= micro_inst(25 DOWNTO 21);
+	SIGNAL micro_addrB	: std_logic_vector(4 DOWNTO 0) 	:= micro_inst(19 DOWNTO 15);
+	SIGNAL muxA		: std_logic 							:= micro_inst(20);			-- 0 then take MIR A or 1 take from %r 
+	SIGNAL muxB		: std_logic 							:= micro_inst(14);
+	SIGNAL muxC		: std_logic 							:= micro_inst(15);			-- To either store in registers or mm
+	SIGNAL rd		: std_logic 							:= micro_inst(12);
+	SIGNAL wr		: std_logic 							:= micro_inst(11);
+	SIGNAL cond		: std_logic_vector(2 DOWNTO 0) 	:= micro_inst(10 DOWNTO 8);
+	SIGNAL jump		: std_logic_vector(7 DOWNTO 0) 	:= micro_inst(7 DOWNTO 0);
 	
-	SIGNAL instr	: 
+	SIGNAL instr	: std_logic_vector(15 DOWNTO 0) 	:= reg(31);
+	SIGNAL 
+	SIGNAL Abus		: std_logic_vector(15 DOWNTO 0) 	:= "0000000000000000";
+	SIGNAL Bbus		: std_logic_vector(15 DOWNTO 0) 	:= "0000000000000000";
+	
+-- MUX DEC
 	SIGNAL mux2decA: std_logic_vector(4 DOWNTO 0)	:= "00000";
-		SIGNAL mux2decA: std_logic_vector(4 DOWNTO 0)	:= "00000";
-=======
+	SIGNAL mux2decB: std_logic_vector(4 DOWNTO 0)	:= "00000";
+	
+	
 	TYPE reg_vector IS ARRAY (0 to 31) OF std_logic_vector(15 DOWNTO 0); --array of 32 16-bit vectors
 	
 	SIGNAL reg : reg_vector; --32 registers of 16 bits
 										--use example: "reg(2) <= a_16_bit_vector" stores the vector in register 2
->>>>>>> 5eb785d629fcf1c670645506ea261df98e6df8db
+
 BEGIN
 
-	-- DecoderA
-	DECODER:PROCESS()
-		BEGIN
-		-- MUX
-			IF muxA THEN
-				addr2dec <= -- from the instruction 
-			ELSE
-				addr2decA <= addrA
-			END IF;
-			IF muxB THEN
-				addr2dec <= -- from the instruction 
-			ELSE
-				addr2decB <= addrB
-			END IF;
-			
-			
-		END PROCESS DECODEA;
-		
 	MUX:PROCESS()
 		BEGIN
+		
+		-- MUX A
+			IF muxA THEN
+				addr2decA <= '0' & instr(12 DOWNTO 9);
+			ELSE
+				addr2decA <= micro_addrA;
+			END IF;
 			
-		END PROCESS MUX;
+		-- MUX B	
+			IF muxB THEN	-- The role of i [instr(13)] depents on OP1
+				IF instr(15 DOWNTO 14) = "00" THEN				-- ARITHMATIC
+					IF NOT(instr(13)) THEN
+						addr2decB <= '0' & instr(3 DOWNTO 0);
+					ELSE
+						Bbus <= "00000000000" & instr(4 DOWNTO 0);
+					END IF;
+					
+				ELSIF instr(15 DOWNTO 14) = "01" THEN			-- MEMORY
+					IF NOT(instr(13)) THEN
+						addr2decB <= "0" & instr(3 DOWNTO 0);
+					ELSE
+						Bbus <= "0000000" & instr(7 DOWNTO 0);
+					END IF;
+					
+				ELSIF instr(15 DOWNTO 14) = "10" THEN			-- DISP
+					END IF;
+					
+				ELSIF instr(15 DOWNTO 14) = "11" THEN			-- BRANCH/SETHI
+					END IF;
+					
+				END IF;
+				
+			ELSE
+				addr2decB <= micro_addrB;
+			END IF;
+	END PROCESS MUX;
 
+	
+	DECODER:PROCESS(addr2decA, addr2decB)
+		BEGIN
+		-- DECODER	set binary addr to integer that points to register
+		IF (instr(15 DOWNTO 13) = "011") OR (instr(15 DOWNTO 13) = "001")
+			Abus <= reg(to_integer(unsigned(addr2decA)));
+		ELSE
+			Abus <= reg(to_integer(unsigned(addr2decA)));
+			Bbus <= reg(to_integer(unsigned(addr2decB)));
+		END PROCESS DECODER:
+			
 END;
