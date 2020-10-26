d986835508bb46afc9ce6067da0537492cf613d5
7kasper <kasper77077@gmail.com>
2020-10-26 15:29:32 +0100

    Add async reset.

--- hal8080.vhd
+++ hal8080.vhd
@@ -12,22 +12,24 @@ ENTITY hal8080 IS
         -- INTERNALS
         clk : IN std_logic; -- 50 Mhz Clock
         -- OUTPUT
-        seg0, seg1 : OUT STD_LOGIC_VECTOR(6 DOWNTO 0); -- first segment display pair
-        seg2, seg3 : OUT STD_LOGIC_VECTOR(6 DOWNTO 0); -- second segment display piar
-        seg4, seg5 : OUT STD_LOGIC_VECTOR(6 DOWNTO 0); -- third segment display piar
-        leds : OUT STD_LOGIC_VECTOR(9 DOWNTO 0); -- leds
+        seg0, seg1 : OUT std_logic_vector(6 DOWNTO 0); -- first segment display pair
+        seg2, seg3 : OUT std_logic_vector(6 DOWNTO 0); -- second segment display piar
+        seg4, seg5 : OUT std_logic_vector(6 DOWNTO 0); -- third segment display piar
+        leds : OUT std_logic_vector(9 DOWNTO 0); -- leds
         -- INPUT
-        switches : IN STD_LOGIC_VECTOR(9 DOWNTO 0); -- user switches
-        buttons : IN STD_LOGIC_VECTOR(3 DOWNTO 0) -- user buttons
+        switches : IN std_logic_vector(9 DOWNTO 0); -- user switches
+        buttons : IN std_logic_vector(3 DOWNTO 0) -- user buttons
     );
 END ENTITY hal8080;
 
 ARCHITECTURE structure OF hal8080 IS
+    SIGNAL reset : std_logic := '1';
 BEGIN
 
     -- setup, used to burn the main memory and setup.
     setup: ENTITY work.setup PORT MAP(
         clk => clk,
+        reset => reset,
         leds => leds
     );
     -- memory, the main memory.
@@ -38,5 +40,7 @@ BEGIN
     processor: ENTITY work.processor PORT MAP(
         clk => clk
     );
+
+    reset <= '0' WHEN buttons = "0000" else '1';
 
 END structure;
--- setup/setup.vhd
+++ setup/setup.vhd
@@ -10,6 +10,7 @@ use IEEE.numeric_std.ALL;
 ENTITY setup IS
     PORT(
         clk : IN std_logic; -- Main (50Mhz) clock.
+        reset : IN std_logic;
         leds : OUT std_logic_vector(9 DOWNTO 0) -- leds
     );
 END ENTITY setup;
@@ -18,9 +19,12 @@ ARCHITECTURE bhv OF setup IS
     SIGNAL cnt : integer RANGE 0 TO 50000000 := 0;
     SIGNAL ledcnt : unsigned(9 downto 0) := (others => '0');
 BEGIN
-    PROCESS(clk)
+    PROCESS(clk, reset)
     BEGIN
-        IF rising_edge(clk) THEN
+        IF reset = '0' THEN
+            cnt <= 0;
+            ledcnt <= "0000000000";
+        ELSIF rising_edge(clk) THEN
             cnt <= cnt + 1;
             IF cnt >= 49999999 THEN
                 ledcnt <= ledcnt + 1;
@@ -31,4 +35,5 @@ BEGIN
             END IF;
         END IF;
     END PROCESS;
+    leds <= std_logic_vector(ledcnt);
 END bhv;
