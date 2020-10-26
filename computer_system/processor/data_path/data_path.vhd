8a5ec691a080994f577f15356de87910d8deb4c5
Nammoyosto <nammoyosto2000@gmail.com>
2020-10-26 12:19:31 +0100

    added ports to data_path

--- computer_system/processor/data_path/data_path.vhd
+++ computer_system/processor/data_path/data_path.vhd
@@ -2,6 +2,15 @@ LIBRARY IEEE;
 USE IEEE.std_logic_1164.ALL;
 ENTITY data_path IS
 	PORT(
+		clk			: IN 	std_logic;
+		reset			: IN 	std_logic;
+		instruction : IN 	std_logic_vector(15 DOWNTO 0);
+		mmI			: IN 	std_logic_vector(15 DOWNTO 0);
+		statusN		: OUT std_logic;
+		statusZ		: OUT std_logic;
+		mmAdress		: OUT std_logic_vector(15 DOWNTO 0);
+		mmData		: OUT std_logic_vector(15 DOWNTO 0);
+		ir				: OUT integer -- have to set a range
 		
 	);
 END ENTITY data_path;
