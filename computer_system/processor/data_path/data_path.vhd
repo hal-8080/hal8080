f61336e39690f31a4bf0b5b288e24596d53c3642
Nammoyosto <nammoyosto2000@gmail.com>
2020-10-27 12:03:26 +0100

    added cmux and fixed overlapping signal assignments

--- computer_system/processor/data_path/data_path.vhd
+++ computer_system/processor/data_path/data_path.vhd
@@ -1,4 +1,4 @@
---clear drivers (move all bus assignments to the decoder process)
+--clear drivers (move random gen to alu)
 --try moving the alu to a different entity
 --
 --
@@ -30,7 +30,7 @@ ARCHITECTURE bhv OF data_path IS
 	
 	TYPE reg_vector IS ARRAY (0 to 31) OF std_logic_vector(15 DOWNTO 0); --array of 32 16-bit vectors
 
-	SIGNAL reg : reg_vector; --32 registers of 16 bits --use example: "reg(2) <= a_16_bit_vector" stores the vector in register 2
+	SIGNAL reg : reg_vector := (others=>(x"0000")); --32 registers of 16 bits --use example: "reg(2) <= a_16_bit_vector" stores the vector in register 2
 	
 									
 -- Split up the micro instruction									
@@ -45,7 +45,7 @@ ARCHITECTURE bhv OF data_path IS
 	SIGNAL ALU		: std_logic_vector(3 DOWNTO 0)	:= micro_inst(14 DOWNTO 11);
 	SIGNAL cond		: std_logic_vector(2 DOWNTO 0) 	:= micro_inst(10 DOWNTO 8);
 	SIGNAL jump		: std_logic_vector(7 DOWNTO 0) 	:= micro_inst(7 DOWNTO 0);
-	
+--	
 	SIGNAL instr	: std_logic_vector(15 DOWNTO 0) 	:= reg(31);
 	
 	SIGNAL Abus		: std_logic_vector(15 DOWNTO 0) 	:= x"0000";
@@ -59,22 +59,10 @@ ARCHITECTURE bhv OF data_path IS
 	SIGNAL addr2decB : std_logic_vector(7 DOWNTO 0) := x"00";
 
 -- from alu & memory to register
-	SIGNAL ALUout	: signed(17 DOWNTO 0);
-	SIGNAL ALUbool	: boolean;
-	SIGNAL Big_temp_reg: std_logic_vector(15 DOWNTO 0):= x"0000";
-	SIGNAL temp_reg: std_logic_vector(7 DOWNTO 0)	:= x"00";
-	
---status bits
-	
-	--SIGNAL status	: std_logic_vector(1 DOWNTO 0); --vector of status bits ZN
+	SIGNAL ALUout	: signed(15 DOWNTO 0);
 	
 
 BEGIN
-	statusZ <= ALUout(16);
-	statusN <= ALUout(17);
-	--reg(0) <= x"0000";-- make sure reg(0) is always 0
-	
-	
 	
 	--A MUX & B MUX
 	MUX:PROCESS(clk, reset)
@@ -106,31 +94,10 @@ BEGIN
 					
 				ELSIF instr(15 DOWNTO 14) = "10" THEN			-- DISP
 					
-					
-				ELSIF instr(15 DOWNTO 14) = "11" THEN			-- BRANCH/SETHI
-					IF instr(13) = '0' THEN							-- Branch
-						-- PC needs to be updated?
-					ELSE													-- Set-hi/low
-						IF instr(8) = '0' THEN						-- high
-							big_temp_reg <= reg(to_integer(unsigned(addr2decA)));
-							temp_reg <= big_temp_reg(7 DOWNTO 0);
-							reg(to_integer(unsigned(addr2decA))) <= instr & temp_reg;
-						ELSE												-- low
-							big_temp_reg <= reg(to_integer(unsigned(addr2decA)));
-							temp_reg <= big_temp_reg(15 DOWNTO 8);
-							reg(to_integer(unsigned(addr2decA))) <= temp_reg & instr;
-						END IF;
-					END IF;
 				END IF;
 				
 			ELSE
 				addr2decB <= "000" & micro_addrB;
-			END IF;
-			-- MUX C
-			IF (muxc = '1') THEN		-- From ALU
-				Cbus <= std_logic_vector(ALUout(15 DOWNTO 0));
-			ELSE					-- From MM
-				Cbus <= mmI;
 			END IF;
 		END IF;
 	END PROCESS MUX;
@@ -144,23 +111,24 @@ BEGIN
 			IF (instr(15 DOWNTO 13) = "011") OR (instr(15 DOWNTO 13) = "001") THEN		-- For ALU and MEM when i '1'
 				Abus <= reg(to_integer(unsigned(addr2decA))); --Abus<=reg(A)
 				Bbus <= reg(to_integer(unsigned(addr2decA))); --Bbus<=reg(B)
-			ELSIF (instr(15 DOWNTO 14) = "000") OR (instr(15 DOWNTO 14) = "010") THEN	-- For ALU and MEM when i '0'
+			ELSIF (instr(15 DOWNTO 13) = "000") OR (instr(15 DOWNTO 13) = "010") THEN	-- For ALU and MEM when i '0'
 				Abus <= reg(to_integer(unsigned(addr2decA)));
 				Bbus <= reg(to_integer(unsigned(addr2decB)));
 			END IF;
 			
-			reg(to_integer(unsigned(addr2decA))) <= Cbus;
 		END IF;
 	END PROCESS DECODER;
 		
 	--THE ALU	
-
 	Maths:PROCESS(clk,reset)
 		CONSTANT	max_value				: integer := 1024;
 		VARIABLE Abus_sign, Bbus_sign : signed(15 DOWNTO 0);
 		VARIABLE abus_int, Bbus_int	: integer RANGE -32768 TO 32767;
 		VARIABLE powTemp					: integer RANGE -32768 TO 32767;
 		VARIABLE solution					: signed(15 DOWNTO 0);
+		--random gen
+		CONSTANT seed 			: unsigned(15 DOWNTO 0) := x"ABCD";	--starting seed 
+		VARIABLE random 		: unsigned(15 DOWNTO 0) := seed;
 	BEGIN
 		IF reset = '0' THEN
 		ELSIF rising_edge(clk) THEN
@@ -193,58 +161,86 @@ BEGIN
 					END IF;
 				END LOOP;
 				
-			WHEN x"C" => 																	--EQL
+			WHEN x"C" => 																	--EQL true is denoted as hex FFFF and false as hex 0000
 				IF Abus = Bbus THEN
-					ALUbool <= TRUE;
+					ALUout <= x"FFFF";
 				ELSE
-					ALUbool <= FALSE;
+					ALUout <= x"0000";
 				END IF;
 				
-			WHEN x"D" =>																	--GT
+			WHEN x"D" =>																	--GT  true is denoted as hex FFFF and false as hex 0000
 				IF Abus > Bbus THEN
-					ALUbool <= TRUE;
+					ALUout <= x"FFFF";
 				ELSE
-					ALUbool <= FALSE;
+					ALUout <= x"0000";
 				END IF;
 				
 			WHEN x"E" =>	solution := Bbus_sign;										--COPY
-			WHEN OTHERS => null;															-- RAND (SEPERATE PROCESS)
+			
+			WHEN x"F" => ALUout <= signed(random);											-- RAND (SEPERATE in the process)
+			WHEN OTHERS => null;
 			
 			END CASE;
-				ALUout(15 DOWNTO 0) <= solution;
-				IF to_integer(ALUout(15 DOWNTO 0)) = 0 THEN
-					ALUout(16) <= '1'; --statusZ
-					ALUout(17) <= '0'; --statusN
-				ELSIF to_integer(ALUout(15 DOWNTO 0)) < 0 THEN
-					ALUout(16) <= '0'; --statusZ
-					ALUout(17) <= '1'; --statusN
-				ELSE
-					ALUout(16) <= '0'; --statusZ
-					ALUout(17) <= '0'; --statusN
-				END IF;
-		END IF;
-	END PROCESS Maths;
-	
-	--pseudo random generator (ALU ="F")
-	RANDO:PROCESS (clk, reset)
-		CONSTANT seed 			: unsigned(15 DOWNTO 0) := x"ABCD";	--starting seed 
-		VARIABLE random 		: unsigned(15 DOWNTO 0) := seed;
-	BEGIN
-		IF reset = '0' THEN
-			random := seed;
-		ELSIF rising_edge(clk) THEN
-			--multiplying random with seed and store the lower 16 bits in random every clock cycle
+			
 			IF random = x"0000" THEN		
 				random := seed;
 			ELSE
 				random := RESIZE((random * seed), 16);
 			END IF;
-			--when ALU is x"F", send the current random value to the mux
-			IF ALU = x"F" THEN
-				ALUout(15 DOWNTO 0) <= signed(random);
-			END IF;
+			
+			
+				ALUout <= solution;
+				IF to_integer(ALUout) = 0 THEN
+					statusZ <= '1'; --statusZ
+					statusN <= '0'; --statusN
+				ELSIF to_integer(ALUout) < 0 THEN
+					statusZ <= '0'; --statusZ
+					statusN <= '1'; --statusN
+				ELSE
+					statusZ <= '0'; --statusZ
+					statusN <= '0'; --statusN
+				END IF;
 		END IF;
-	END PROCESS RANDO;
+	END PROCESS Maths;
+	
+	
+	--move the ALU output or mm data to the Cbus & sethigh/low
+	----try changing op1 to a case statement
+	CMUX:PROCESS (clk, reset)	
+		VARIABLE temp_reg			: std_logic_vector(15 DOWNTO 0);
+	BEGIN
+	IF reset = '0' THEN
+	ELSIF rising_edge(clk) THEN
+		
+		IF instr(15 DOWNTO 14) = "11" THEN			-- BRANCH/SETHI
+			IF instr(13) = '0' THEN							-- Branch
+				-- PC needs to be updated?
+			ELSE													-- Set-hi/lo
+				IF instr(8) = '0' THEN						-- high
+					Cbus <= instr(7 DOWNTO 0) & Abus(7 DOWNTO 0);
+				ELSE												-- low
+					Cbus <= Abus(15 DOWNTO 8) & instr(7 DOWNTO 0);
+				END IF;
+			END IF;
+			--store ALU/MM in Cbus
+		ELSIF instr(15 DOWNTO 14) = "00" THEN
+			Cbus <= std_logic_vector(ALUout);
+		ELSIF instr(15 DOWNTO 14) = "01" THEN
+			Cbus <= mmI;
+		END IF;
+	END IF;
+	END PROCESS CMUX;
+	
+--	CTOREG:PROCESS(clk, reset)
+--	BEGIN
+--		IF reset = '0' THEN
+--		ELSIF rising_edge(clk) THEN
+--			reg(0) <= x"0000";-- make sure reg(0) is always 0
+--			reg(to_integer(unsigned(addr2decA))) <= Cbus;
+--		END IF;
+--	END PROCESS CTOREG;
+	
+	
 	
 	
 END;
