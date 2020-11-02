f238562ed2245be7ee3154e53a3601882fd69a69
7kasper <kasper77077@gmail.com>
2020-11-02 16:37:50 +0100

    Comment on first address aligned.

--- assembler/assembler.js
+++ assembler/assembler.js
@@ -449,8 +449,8 @@ async function assemble(input, startAddress = 0, percentDone = ()=>{}, warning =
         // Memory is in Little Endian.
         let low = numToHex(parseInt(data[i], 2) >>> 8, 2); // Lowest 8 bits in memory 1
         let hi  = numToHex(parseInt(data[i], 2) & 255, 2); // Lowest 8 bits in memory 0
-        memory.push(`${numToHex(address,   4)}-${ hi}`);
-        memory.push(`${numToHex(address+1, 4)}-${low}${dataComment[i]}`);
+        memory.push(`${numToHex(address,   4)}-${ hi}${dataComment[i]}`);
+        memory.push(`${numToHex(address+1, 4)}-${low}`);
         address += 2;
         percentDone(75 + (25/data.length) * i);
     }
